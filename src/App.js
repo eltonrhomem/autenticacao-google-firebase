74c875547d63cfb23446f1a4a88abfe2cec30bbf
Elton Rodrigues Homem <dataserv.elton@gmail.com>
2022-12-12 12:40:42 -0300

    feat: cria autenticação com google com firebase

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
 import logo from './logo.svg';
 import './App.css';
+import {auth} from './firebase.js'
+
+// import { GoogleAuthProvider } from "firebase/auth";
+import {  signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+
+
 
 function App() {
+  const provider = new GoogleAuthProvider();
+  provider.addScope('https://www.googleapis.com/auth/contacts.readonly');
+  provider.setCustomParameters({
+    'login_hint': 'user@example.com'
+  });
+
+  function makeLogin() {
+      signInWithPopup(auth, provider)
+        .then((result) => {
+          // This gives you a Google Access Token. You can use it to access the Google API.
+          const credential = GoogleAuthProvider.credentialFromResult(result);
+          const token = credential.accessToken;
+          // The signed-in user info.
+          const user = result.user;
+          console.log('user', user)
+          // ...
+        }).catch((error) => {
+          // Handle Errors here.
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          // The email of the user's account used.
+          const email = error.customData.email;
+          // The AuthCredential type that was used.
+          const credential = GoogleAuthProvider.credentialFromError(error);
+          // ...
+        });
+
+  }
+
+ 
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={makeLogin}>Login com google</button>
     </div>
   );
 }
---
+++ src/firebase.js
@@ -0,0 +1,24 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAZRPmm549K1d0r-8LQUPc7eGPmDi1-TEg",
+  authDomain: "autenticacao-cbd5e.firebaseapp.com",
+  projectId: "autenticacao-cbd5e",
+  storageBucket: "autenticacao-cbd5e.appspot.com",
+  messagingSenderId: "832446356767",
+  appId: "1:832446356767:web:c1e8e70d0a0a913819642a",
+  measurementId: "G-GKSRM3WM9C"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+
+export const auth = getAuth();
